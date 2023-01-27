190008b54736e325e904887cb4e8105ea161d2b5
N1M3Xx <119055821+N1M3Xx@users.noreply.github.com>
2023-01-27 17:03:54 +0100

    .

---
+++ 27_01/es1.js
@@ -0,0 +1,41 @@
+const http = require('http');
+const q = require('querystring');
+
+const HOSTNAME = '127.0.0.1';
+const PORT = process.env.PORT || 3000;
+
+var url = require('url');
+
+const server = http.createServer((req, res) => {
+
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'application/json');
+   var uriReq = url.parse(req.url);
+  var email = q.parse(uriReq.query);
+  var isValidEmail = valida(email.email)
+  var risposta = {
+    "email": isValidEmail
+  }
+
+  console.log(risposta);
+  res.end(JSON.stringify(risposta))
+  
+});
+
+server.listen(PORT, HOSTNAME, () => {
+  console.log('Server running');
+});
+
+function valida(s) {
+  if(s==undefined || s==null) {
+    return false
+  }
+  var registro = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+  if (s.match(registro)) {
+    return true;
+  } 
+  else {
+    return false;
+  }
+}
--- index.js
+++ index.js
@@ -3,11 +3,33 @@ const http = require('http');
 const HOSTNAME = '127.0.0.1';
 const PORT = process.env.PORT || 3000;
 
+var url = require('url');
+var adr = 'http://localhost:8080/default.htm?year=2017&month=february';
+var q = url.parse(adr, true);
+var a = []
+
 const server = http.createServer((req, res) => {
 
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('HELLO World!\n');
+  if(req.url == "/nome")
+    res.end("Gonzalo");
+  else if(req.url == "/cognome") 
+    res.end('Perez Brisco');
+  else if(req.url == "/data")
+    res.end('16/12/2002');
+  else if(req.url == "/add")
+    {
+      a.append(q.query.stringa)
+      res.end(a)
+    }
+    else if(req.url == "/remove")
+    {
+      a.remove(q.query.stringa)
+      res.end(a)
+    }
+    else
+      res.end('Ciao Mondo!');
 });
 
 server.listen(PORT, HOSTNAME, () => {
