e92b881938f4638e9ce1b2e3312c91e9469a6bef
Gonzalo Perez Brisco <119055821+N1M3Xx@users.noreply.github.com>
2023-02-10 16:36:35 +0100

    Update es1.js

--- 10_02/es1.js
+++ 10_02/es1.js
@@ -8,17 +8,33 @@ var url = require('url');
 
 const server = http.createServer((req, res) => {
 
-  console.log(1)
+  var a = []
+  var s = true
 
   req.on('data', (data) => {
-    console.log(2)
+    if(req.url == "/add")
+    {
+      for(x = 0; x < a.length; x++){
+        if(a[x] == data)
+          s = false
+      }
+      if(s == true)
+        a.push(data)
+    }
+    else if(req.url == "/remove")
+    {
+      for(x = 0; x < a.length; x++){
+        if(a[x] == data){
+          a.splice(x, 1)
+        }
+      }
+    }
   })
   
   req.on('end', () => {
-    console.log(3)
+    console.log(a)
   })
 
-  console.log(4)
 });
 
 server.listen(PORT, HOSTNAME, () => {
